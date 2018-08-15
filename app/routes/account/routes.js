536abaaa5c18734e7670adbe0bd5951095c31d9e
Dan Lacey <dan@danlaceyltd.co.uk>
2018-08-15 09:38:17 +0100

    Pages created to support account signin / activation journey

--- app/routes/account/routes.js
+++ app/routes/account/routes.js
@@ -58,11 +58,9 @@ module.exports = function(router) {
 
       'layout': '2-0',
       'h1': 'Create an account',
-      'captionXL': 'Creating an account makes it easy to complete job applications, track your application progress and receive notifications and job alerts by email.',
-
-
       'form': {
         'inputs': [
+
 
           {
             'type': 'text',
@@ -73,12 +71,13 @@ module.exports = function(router) {
             'width': '20'
           },
 
+
           {
             'type': 'text',
             'name': 'accountEmail',
             'id': 'accountEmail',
             'label': 'Email address',
-            'hint': 'We will use this email address to sign you in to this service, send you job alerts and notifications about your job applications',
+            'hint': 'If you are a civil servant and you would like to view and apply for internal jobs, we recommend you create an account with your work email address',
             'errorText': 'You must enter a valid email address',
             'width': '20'
           },
@@ -99,46 +98,9 @@ module.exports = function(router) {
             'label': 'Repeat password',
             'errorText': 'Please enter a valid password',
             'width': '20'
-          },
-
-          {
-            'type': 'conditionallyRevealMultiInput',
-            'groupName': 'propertySearchOptions',
-            'label': 'Are you currently employed as a civil servant?',
-            'required': true,
-            'errorText': 'Please select an option',
-            'radios': [{
-                'id': 'yesCS',
-                'label': 'Yes, I am a civil servant',
-                'value': 'yesCS',
-                'name': 'yesCS',
-                'concealedInputs': [{
-                  'concealedLabel': 'Your work email address',
-                  'concealedName': 'csEmailAddress',
-                  'concealedId': 'csEmailAddress',
-                  'concealedType': 'text',
-                  'concealedHint': 'Provide your work email address to view and apply for internal job vacancies. We will send an activation email to this address. If you cannot access your work address, you can provide your manager\'s email address'
-
-
-                }]
-
-              },
-              {
-                'id': 'noCS',
-                'label': 'No, I am not a civil servant',
-                'value': 'noCS',
-                'name': 'noCS',
-                'checked': 'checked'
-              }
-
-
-            ]
-
-          }, {
-            'type': 'details',
-            'summary': 'Why do you need this information?',
-            'text': '<p class="govuk-body">If you are a civil servant:</p><ul class="govuk-list govuk-list--bullet"><li>If you want to view internal jobs and have a UK government work-related email address, then you can view and apply for internal jobs.</li><li>The information is used only for this purpose.</li><li>If you do not have access to your work email address you can provide the email address of your manager.</li></ul><p class="govuk-body">If you are not a civil servant:</p><ul class="govuk-list govuk-list--bullet"><li>You will not be able to view or apply for internal job vancancies.</li><li>For more information, see the Civil Service\'s <a href="#">Internal jobs policy</a>'
           }
+
+
         ],
 
         'buttonText': 'Create account',
@@ -153,28 +115,34 @@ module.exports = function(router) {
     res.render('layoutBuilder.html', {
       'layout': '2-0',
       'h1': 'Check your email',
-      'captionXL': 'We have sent an activation email to your email address: itsme@myorganisation.co.uk. Please check your email inbox and click the link to activate your account.',
+
+
 
 
       'form': {
         'inputs': [
+          {
+            'type' : 'html',
+            'html' : '<p class="govuk-body-l">Activate your account by following the instructions sent to your email address: itsme@myorganisation.co.uk</p>'},
 
 
 
          {
             'type': 'details',
-            'summary': 'I haven\'t received my activation link',
+            'summary': 'I haven\'t received my activation email',
             'text': '<p class="govuk-body">It may take a few minutes for the email to arrive. Please check your email spam  or junk mail folder. If you still haven\t received your email after a short while, you can  <a href="#">re-send activation link</a>.'
           },
 
         ],
 
         'buttonText': 'Search for a job',
-        'buttonUrl': '../search'
+        'buttonUrl': '../search/results/index-signedin'
       }
 
     })
   })
+
+
 
 
 }
--- app/routes/internal-jobs/routes.js
+++ app/routes/internal-jobs/routes.js
@@ -5,31 +5,67 @@ module.exports = function(router) {
 
   router.get('/internal-jobs/', function (req, res) {
 
-res.render('./internal-jobs/index.html',{
+res.render('layoutBuilder.html',{
 
 
 
   'h1': 'Access internal jobs',
         'form': {
           'inputs': [
+            {
+                'type': 'html',
+                'html': '<p class="govuk-body-l">Civil servants can view and apply for internal jobs by providing their work email address. An email will be sent to your work email address.</p><p class="govuk-body">If you cannot access your work email address, you can provide the email address of your line manager to allow access on your behalf.</p>'
+              },
         {
           'type': 'text',
-          'name': 'this is the name',
-          'id': 'this is the ID',
+          'name': 'workEmail',
+          'id': 'workEmail',
           'label': 'Enter your work email address',
-          'hint': 'Must be a valid work email address',
           'errorText': 'You must enter a valid email address'
         }
         ],
- 'backTo': 'http://www.bbc.co.uk/news',
+ 'backTo': '',
  'buttonText' : 'Send activation link',
- 'buttonUrl' : 'http://www.bbc.co.uk/news'
+ 'buttonUrl' : '/internal-jobs/activate'
 }
 
 }
     )
   }
 )
+
+
+router.get('/internal-jobs/activate', function(req, res) {
+
+  res.render('layoutBuilder.html', {
+    'layout': '2-0',
+    'h1': 'Check your email',
+    'form': {
+      'inputs': [
+        {
+          'type' : 'html',
+          'html' : '<p class="govuk-body-l">Verify your work email address by following the instructions sent to myworkaddress@governmentaddress.gov.uk</p>'},
+
+
+
+       {
+          'type': 'details',
+          'summary': 'I haven\'t received my verification email',
+          'text': '<p class="govuk-body">It may take a few minutes for the email to arrive. Please check your email spam or junk mail folder. If you still haven\t received your email after a short while, you can  <a href="#">re-send activation link</a>.'
+        },
+
+      ],
+
+      'buttonText': 'Search for a job',
+      'buttonUrl': '../search/results/index-signedin'
+    }
+
+  })
+})
+
+
+
+
 
 
   }
