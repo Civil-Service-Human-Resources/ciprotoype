53f2c36662172159f0738133b33f8ad17c2f04d5
Grant Bajere <grantbajere@pixelfunnel.co.uk>
2018-08-22 10:50:37 +0100

    dead code removed. fixed js errors

--- app/routes/search/routes.js
+++ app/routes/search/routes.js
@@ -1,83 +1,48 @@
 // Search
+module.exports = function (router) {
+  router.get('/search', function (req, res) {
+    let emailAddress = req.session.data['accountEmail']
+    let isCS = 'no'
+    if (typeof emailAddress !== 'undefined') {
+      isCS = emailAddress.includes('gov')
+    }
 
-var loadJobListing = require('../../data/jobListing')
-module.exports = function(router) {
+    res.render('layoutBuilder.html', {
 
-
-
-  router.get('/search/', function (req, res) {
-    var emailAddress = req.session.data['accountEmail']
-    var signedIn = req.session.data['hasSignedIn']
-    var isCS = 'no'
-      if (typeof emailAddress !== 'undefined') {
-    var isCS = emailAddress.includes('gov');
-  }
-
-res.render('layoutBuilder.html',{
-
-  'h1': 'Job search',
-  'captionXL' : 'Search and apply for jobs within the Civil Service and central government organisations',
-  'email' : emailAddress,
-  'cs' : isCS,
-        'form': {
-          'action': '../search/results',
-          'inputs': [
-        {
-          'type': 'text',
-          'name': 'keyword',
-          'id': 'keyword',
-          'label': 'Keyword (optional)',
-          'hint': 'Enter a job title or skill',
-          'errorText': 'You must enter a valid email address',
-          'width' : '20'
-        },
-        {
-          'type': 'text',
-          'name': 'location',
-          'id': 'location',
-          'label': 'Location (optional)',
-          'errorText': 'You must enter a valid email address',
-          'width' : '20'
-        }
+      'h1': 'Job search',
+      'captionXL': 'Search and apply for jobs within the Civil Service and central government organisations',
+      'email': emailAddress,
+      'cs': isCS,
+      'form': {
+        'action': '../search/results',
+        'inputs': [
+          {
+            'type': 'text',
+            'name': 'keyword',
+            'id': 'keyword',
+            'label': 'Keyword (optional)',
+            'hint': 'Enter a job title or skill',
+            'errorText': 'You must enter a valid email address',
+            'width': '20'
+          },
+          {
+            'type': 'text',
+            'name': 'location',
+            'id': 'location',
+            'label': 'Location (optional)',
+            'errorText': 'You must enter a valid email address',
+            'width': '20'
+          }
 
         ],
 
- 'buttonText' : 'Search',
- 'buttonUrl' : '../search/results'
-}
+        'buttonText': 'Search',
+        'buttonUrl': '../search/results'
+      }
 
-}
-
-
+    }
 
     )
+  })
 
-
-
-
-  }
-)
-
-router.get('/search/results', function (req, res) {
-  var emailAddress = req.session.data['accountEmail']
-  var signedIn = req.session.data['hasSignedIn']
-  var isCS = 'no'
-    if (typeof emailAddress !== 'undefined') {
-  var isCS = emailAddress.includes('gov');
 }
-
-res.render('./search/results/index.html',{
-'email' : emailAddress,
-'cs' : isCS
-  }
-
-
-  )
-}
-)
-
-
-
-
-
-  }
